ad078bf9835044b47d8505d9e378dac91c6a47bd
RyanNovas <ryan@quill.org>
2015-07-29 17:06:10 -0400

    adds computer opponent

--- pig.rb
+++ pig.rb
@@ -3,7 +3,9 @@ class Pig
 
 def initialize
   @dice_val = 0
-  @score = 0
+  @p1score = 0
+  @p2score = 0
+  @current_player = 'Human'
   roll
 end
 
@@ -14,31 +16,45 @@ def roll
   evaluator 
 end
 
-
 #roll evaluator, with counter
 def evaluator
-  if @dice_val == 1
+  if @dice_val == 1 && @current_player == 'Human'
+    @p1score = 0
     game_over
+  elsif 
+     @dice_val == 1 && @current_player == 'Computer'
+     @p2score = 0
+     game_over
   else
-    @score += @dice_val 
-    puts "Your score is #{@score}. \n Would you like to roll or hold?"
-    answer = gets.chomp.downcase
-      if answer == 'roll'
-        roll
-      else 
-        game_over
-      end
+    if @current_player == 'Human'
+      @p1score += @dice_val 
+      puts "Your score is #{@p1score}. \n Would you like to roll or hold?"
+      answer = gets.chomp.downcase
+        if answer == 'roll'
+          roll
+        else 
+          @current_player = 'Computer'
+          roll
+        end
+    else @p2score += @dice_val
+      choice = rand(0..1)
+      puts "Computer's score is #{@p2score}."
+        if choice == 0
+          puts "Computer holds"
+          @current_player = 'Human'
+          roll
+        else 
+          puts "Computer rolls"
+          roll
+        end
+    end
   end
 end
 
 
 #game over
 def game_over
-  if @dice_val == 1
-    puts "You lose and have no points"
-  else 
-    puts "You have #{@score}"
-  end
+    puts "Human, your score #{@p1score}. Computer, your score is #{@p2score}."
 end
 end #ends class
 
